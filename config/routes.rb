af2930c131eb9a014949c71226b8ed7c7298d072
Bjarne Sievers <jellyfishcoding@live.de>
2016-10-23 20:43:01 +0200

    Added AuthorController. Closes #1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,6 +15,9 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :authors
+  get 'authors/new'
 
   # Example resource route with options:
   #   resources :products do
