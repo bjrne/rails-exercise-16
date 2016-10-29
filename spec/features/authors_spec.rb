640901a9b8fa82fdc6e024c9da6f868d34ee21b9
Bjarne Sievers <jellyfishcoding@live.de>
2016-10-29 23:13:13 +0200

    Failing test for #3.

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -11,4 +11,9 @@ describe "Authors page", :type => :feature do
     visit new_author_path
     expect(page).to have_text("First name")
   end
+
+  it "should have a submit button" do
+    visit new_author_path
+    expect(page).to have_button("Create Author")
+  end
 end
