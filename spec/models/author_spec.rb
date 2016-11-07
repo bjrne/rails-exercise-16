e6436a2fffd249e97933b962d7a3ff8e9a11a11b
Bjarne Sievers <jellyfishcoding@live.de>
2016-11-07 23:36:40 +0100

    Failing test for #5, #7 & #8.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -6,5 +6,18 @@ RSpec.describe Author, type: :model do
       @author = create(:author)
       expect(@author).not_to be_nil
     end
+
+    it "returns the full name" do
+      @author = create(:author)
+      expect(@author.name).to eq(@author.first_name + ' ' + @author.last_name)
+    end
+
+    it "returns 'Alan Turing' for specific test data" do
+      author = Author.new
+      expect(author.name).to be_nil
+      author.first_name = 'Alan'
+      author.last_name = 'Turing'
+      expect(author.name).to eq 'Alan Turing'
+    end
   end
 end
