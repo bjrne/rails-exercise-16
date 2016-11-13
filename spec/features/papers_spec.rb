5681fc5c3b145047fbb6edcfe36bae7630db5e7b
Bjarne Sievers <jellyfishcoding@live.de>
2016-11-13 20:17:37 +0100

    Failing test for #20.

--- spec/features/papers_spec.rb
+++ spec/features/papers_spec.rb
@@ -7,4 +7,12 @@ describe "New papers page", :type => :feature do
     expect(page).to have_text("New Paper")
   end
 
+  it "should show an error message if name fields are empty" do
+    visit new_paper_path
+    click_button :create_paper
+    expect(page).to have_text("Title can't be blank")
+    expect(page).to have_text("Venue can't be blank")
+    expect(page).to have_text("Year can't be blank")
+    expect(page).to have_text("Year is not a number")
+  end
 end
