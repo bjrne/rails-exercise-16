22ffe717b84ba96aaf286e62c9c0525f1f470b68
Bjarne Sievers <jellyfishcoding@live.de>
2016-11-13 21:43:43 +0100

    Failing tests for #23.

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Paper model" do
+    it "saves a new paper" do
+      @paper = FactoryGirl.build(:paper)
+      expect(@paper).not_to be_nil
+    end
+
+    it "doesn't validate paper without name attributes" do
+      paper = Author.new
+      expect(paper).to_not be_valid
+    end
+
+    it "has and belongs to many authors" do
+      @paper = FactoryGirl.build(:paper)
+      expect(@paper.authors).not_to be_nil
+    end
+  end
 end
