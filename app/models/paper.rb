81af4cd43455f35d1493fd022fa73dddbb1f5ef5
Bjarne Sievers <jellyfishcoding@live.de>
2016-11-13 20:17:55 +0100

    Added paper model validation. Closes #20.

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,3 +1,7 @@
 class Paper < ActiveRecord::Base
   belongs_to :author
+
+  validates :title, presence: true
+  validates :venue, presence: true
+  validates :year, presence: true, numericality: { only_integer: true }
 end
